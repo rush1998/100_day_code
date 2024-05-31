7871e533c8d2b14b2d6cb0fde85d402f2684eb0c
rush1998 <rushabh.m.patel1998@gmail.com>
2024-05-30 22:59:26 -0400

    Tax calculator

--- Demo.java
+++ Demo.java
@@ -2,13 +2,16 @@ import java.util.*;
 
 public class Demo {
     public static void main(String args[]){
-    Scanner sc= new Scanner(System.in);
-     int number=sc.nextInt();
-     if (number%2 == 0) {
-      System.out.println( number + " is a even number");      
-     }
-     else{
-      System.out.println( number + " is a odd number");
-     }
-    }
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        if (a>18) {
+            System.out.println("Adult");
+        }
+        else if (a>12 && a<20 ) {
+            System.out.println("Teenagers");
+        }
+        else{
+            System.out.println("Kids");
+        }
+       }
 }
