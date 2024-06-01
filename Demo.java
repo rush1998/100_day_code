fb6f983766027257883b3dccf1637be85333dce7
Rushabh Patel <rushabh.m.patel1998@gmail.com>
2024-06-01 14:26:34 -0400

    Modified Sum file

--- Demo.java
+++ Demo.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Demo {
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        String name = sc.nextLine();
-        System.out.println(name);
+        float dec = sc.nextFloat();
+        System.out.println(dec);
     }    
 }
