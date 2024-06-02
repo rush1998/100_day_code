ae25823d12de4d535fe5506e8af05c7278487697
rush1998 <rushabh.m.patel1998@gmail.com>
2024-06-02 17:06:44 -0400

    space removed from demo.java

--- Demo.java
+++ Demo.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class Demo {
     public static void main(String args[]){
-     Scanner sc= new Scanner(System.in);
+    Scanner sc= new Scanner(System.in);
      int number=sc.nextInt();
      if (number%2 == 0) {
       System.out.println( number + " is a even number");      
