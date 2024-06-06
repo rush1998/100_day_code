68e29bae080a1da5293502c1e57fff200076146e
Rushabh Patel <rushabh.m.patel1998@gmail.com>
2024-06-06 13:21:06 -0400

    added data in readme file

---
+++ Demo.java
@@ -0,0 +1,10 @@
+public class Demo {
+    public static void main(String args[]){
+        int a = 10;
+        int b = 39;
+        String name = "Rushabh";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(name);
+    }    
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # 100_day_code
+
+There are two types of datatypes 
+1. primitive 
+2. non-premitive
