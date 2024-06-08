7d9deb10cd7f9e7dd5f0184f13e1604302b34b44
rush1998 <rushabh.m.patel1998@gmail.com>
2024-06-08 09:02:07 -0400

    Largestofthree.java

--- Demo.java
+++ Demo.java
@@ -2,16 +2,10 @@ import java.util.*;
 
 public class Demo {
     public static void main(String args[]){
-        Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        if (a>18) {
-            System.out.println("Adult");
-        }
-        else if (a>12 && a<20 ) {
-            System.out.println("Teenagers");
-        }
-        else{
-            System.out.println("Kids");
-        }
-       }
+       Scanner sc = new Scanner(System.in);
+       int number = sc.nextInt();
+       
+       String type=(number%2 == 0)? "Even" : "Odd";
+       System.out.println(type);
+}
 }
