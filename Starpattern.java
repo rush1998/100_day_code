811abf42bc762e6625beb9601b20218964f3a0bf
rush1998 <rushabh.m.patel1998@gmail.com>
2024-06-12 12:20:52 -0400

    Starpattern.java

---
+++ Starpattern.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Starpattern {
+    public static void main(String args[]){
+        // for(int line=1; line<=4; line++){
+        //     for(int star=1; star<=line; star++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // int n=4;
+        // for(int line=1; line<=n; line++){
+        //     for(int star=1; star<=n-line+1; star++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        int n=4;
+        for(int line=1; line<=n; line++){
+            for(int num=1; num<=line; num++){
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }    
+}
